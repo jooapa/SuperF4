bb82b61b943c890659373a3a2fcf80ad688b552f
jooapas <jooa.akonpelto@gmail.com>
2023-06-21 15:07:53 +0300

    no luck

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,15 +1,31 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use std::{thread::sleep, time::Duration};
+
+use inputbot::{KeybdKey::*, MouseButton::*};
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+
+
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn cps(name: &str) -> String {
+    let formatted = format!("1 Click per {}ms", name);
+    println!("{}", name);
+    formatted // Return the formatted string
+}
+
+#[tauri::command]
+fn startStopClicker(cpms: i32) {
+    println!("Start Clicker: {}ms", cpms);
+    // Rest of your code...
 }
 
 fn main() {
+    let mut cpms: i32 = 0;
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![cps])
+        .invoke_handler(tauri::generate_handler![startStopClicker])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/main.js
+++ src/main.js
@@ -1,18 +1,30 @@
 const { invoke } = window.__TAURI__.tauri;
 
-let greetInputEl;
+let msInterval;
 let greetMsgEl;
 
 async function greet() {
   // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-  greetMsgEl.textContent = await invoke("greet", { name: greetInputEl.value });
+  greetMsgEl.textContent = await invoke("cps", { name: msInterval.value });
 }
 
 window.addEventListener("DOMContentLoaded", () => {
-  greetInputEl = document.querySelector("#greet-input");
+  msInterval = document.querySelector("#ms-input");
   greetMsgEl = document.querySelector("#greet-msg");
   document.querySelector("#greet-form").addEventListener("submit", (e) => {
     e.preventDefault();
     greet();
   });
 });
+
+document.querySelector("#startButton").addEventListener("click", () => { 
+  invoke("startStopClicker");
+  //stop start
+  if (document.querySelector("#startButton").textContent == "Start") {
+    document.querySelector("#startButton").textContent = "Stop";
+  }
+  else {
+    document.querySelector("#startButton").textContent = "Start";
+  }
+}
+);
