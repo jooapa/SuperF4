55ca6b70fbb2f221e1e46909c3d85159a519afba
jooapas <jooa.akonpelto@gmail.com>
2023-06-25 00:58:03 +0300

    working backend

--- src/main.js
+++ src/main.js
@@ -1,34 +1,51 @@
-// const { invoke } = window.__TAURI__.tauri;
+const { invoke } = window.__TAURI__.tauri;
 
 let exeName;
-let greetMsgEl;
-
-// async function greet() {
-//   // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-//   greetMsgEl.textContent = await invoke("cps", { name: exeName.value });
-// }
+var regex = /\.exe$/;
 
 window.addEventListener("DOMContentLoaded", () => {
   exeName = document.querySelector("#ms-input");
-  greetMsgEl = document.querySelector("#greet-msg");
-  document.querySelector("#greet-form").addEventListener("submit", (e) => {
+  document.querySelector("#add-form").addEventListener("submit", (e) => {
     e.preventDefault();
-    // greet();
 
-    var regex = /\.exe$/;
-    var endsWithExe = regex.test(exeName);
+    //check if exeName already exists
+    let element = document.getElementById(exeName.value);
+    if (element !== null) {
+      document.querySelector("#errorId").textContent =
+        "Executable is already in the list";
+      return;
+    }
+
+    //check if exeName ends with .exe
+    var endsWithExe = regex.test(exeName.value);
 
     // Check the result
     if (endsWithExe) {
-      var divElement = document.createElement("div");
-      divElement.className = "blacklistDiv";
-      divElement.textContent = exeName.value;
-      var containerElement = document.querySelector(".container");
-      containerElement.appendChild(divElement);
+      addExeDiv(exeName.value);
+
     } else {
-      document.querySelector("#errorId").textContent = "Executable name must end with .exe";
-    }    
+      document.querySelector("#errorId").textContent =
+        "Executable name must end with .exe";
+    }
   });
 });
 
+function addExeDiv(paraexe) {
+      var divElement = document.createElement("div");
+      divElement.className = "blacklistDiv";
+      divElement.id = paraexe;
+      divElement.textContent = paraexe;
+      divElement.onclick = function () {
+        removeExeDiv(this);
+      };
+      var containerElement = document.querySelector(".container");
+      containerElement.appendChild(divElement);
+}
 
+function removeExeDiv(element) {
+  var divName = element.id;
+  console.log(divName);
+  //delete div with id = divName
+  var divElement = document.getElementById(divName);
+  divElement.parentNode.removeChild(divElement);
+}
